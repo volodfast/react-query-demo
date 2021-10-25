27a1a9fa8365becc7a25981750387aaa333ad6b1
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-26 00:11:47 +0300

    Update App component

--- src/modules/core/components/App/App.tsx
+++ src/modules/core/components/App/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // components
-import CharacterList from 'modules/characters/components/CharacterList';
+import Header from 'modules/core/components/Header';
+import CharacterList from 'pages/CharacterList';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="characters">Characters</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>App wrapper</div>
+      <Header />
       <Switch>
         <Route path="/" exact>
-          <div>Home page</div>
+          <Home />
         </Route>
         <Route path="/characters">
           <CharacterList />
