d24f95a57e167d5cdea79d987b6109c7a5f91124
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-26 00:39:58 +0300

    Update useCharacterList with interfaces

--- src/modules/characters/hooks/useCharacterList.tsx
+++ src/modules/characters/hooks/useCharacterList.tsx
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+// interfaces
+import { ApiListResponse } from 'modules/core/core.interfaces';
+import { Character } from '../character.interfaces';
 
 export const useCharacterList = () => {
   return useQuery('characters', () =>
     axios
-      .get('https://rickandmortyapi.com/api/character')
+      .get<ApiListResponse<Character[]>>(
+        'https://rickandmortyapi.com/api/character'
+      )
       .then((res) => res.data)
   );
 };
