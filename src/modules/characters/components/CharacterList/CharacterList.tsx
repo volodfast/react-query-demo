8d37fa84c2822856e9d8090570921faf4e5784e1
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-25 23:58:42 +0300

    Add CharacterList component

---
+++ src/modules/characters/components/CharacterList/CharacterList.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+// styles
+import { CharacterListContainer } from './CharacterList.styled';
+
+const CharacterList: FC = () => {
+  return <CharacterListContainer>Character list</CharacterListContainer>;
+};
+
+export default CharacterList;
