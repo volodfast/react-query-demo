879c7d1b12b578af224ee3d43b57127f5687ccd3
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-26 00:06:05 +0300

    Add Home page component

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+// styles
+import { HomePageContainer } from './Home.styled';
+
+const HomePage: FC = () => {
+  return <HomePageContainer>Home page</HomePageContainer>;
+};
+
+export default HomePage;
