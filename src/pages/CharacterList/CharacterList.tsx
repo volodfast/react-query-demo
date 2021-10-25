b06753d0aac9ca642cbf932cb431687a7dc97c3b
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-26 00:08:22 +0300

    Add CharacterList page component

---
+++ src/pages/CharacterList/CharacterList.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+// styles
+import { CharacterListPageContainer } from './CharacterList.styled';
+
+const CharacterListPage: FC = () => {
+  return (
+    <CharacterListPageContainer>Character List</CharacterListPageContainer>
+  );
+};
+
+export default CharacterListPage;
