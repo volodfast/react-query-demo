d2ef98cda37b710b95fa63a4153d41ce48e3c94e
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-26 00:26:34 +0300

    Configure react-query provider

--- src/modules/core/components/App/App.tsx
+++ src/modules/core/components/App/App.tsx
@@ -1,22 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 // components
 import Header from 'modules/core/components/Header';
 import CharacterListPage from 'pages/CharacterListPage';
 import HomePage from 'pages/HomePage';
 
+const client = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/characters">
-          <CharacterListPage />
-        </Route>
-      </Switch>
-    </Router>
+    <QueryClientProvider client={client}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/characters">
+            <CharacterListPage />
+          </Route>
+        </Switch>
+        <ReactQueryDevtools />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
