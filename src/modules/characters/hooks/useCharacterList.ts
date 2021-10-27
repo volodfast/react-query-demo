f70f764cb2657b705955f885ec07a828227ce009
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-27 23:53:19 +0300

    Update useCharacterList hook to pass page argument

--- src/modules/characters/hooks/useCharacterList.ts
+++ src/modules/characters/hooks/useCharacterList.ts
@@ -4,11 +4,11 @@ import { useQuery } from 'react-query';
 import { ApiListResponse } from 'modules/core/core.interfaces';
 import { Character } from '../character.interfaces';
 
-export const useCharacterList = () => {
-  return useQuery('characters', () =>
+export const useCharacterList = ({ page }: { page: number }) => {
+  return useQuery(['characters', 'page', page], () =>
     axios
       .get<ApiListResponse<Character[]>>(
-        'https://rickandmortyapi.com/api/character'
+        `https://rickandmortyapi.com/api/character?page=${page}`
       )
       .then((res) => res.data)
   );
