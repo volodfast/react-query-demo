47291f74a1b54c41e6b4acdf17c1909ed271333b
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-27 23:11:35 +0300

    Add link to CharacterListItem component for each character

--- src/modules/characters/components/CharacterList/CharacterListItem/CharacterListItem.tsx
+++ src/modules/characters/components/CharacterList/CharacterListItem/CharacterListItem.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 // interfaces
 import { CharacterListItemProps } from './CharacterListItem.interfaces';
 // styles
@@ -7,7 +8,10 @@ import { CharacterListItemContainer } from './CharacterListItem.styled';
 const CharacterListItem: FC<CharacterListItemProps> = ({ item }) => {
   return (
     <CharacterListItemContainer>
-      {`Name: ${item.name}`}
+      <div>{`Name: ${item.name}`}</div>
+      <div>
+        <Link to={`/characters/${item.id}`}>Link</Link>
+      </div>
     </CharacterListItemContainer>
   );
 };
