d824df82aec7bd56bd6e45d4c21e5c89f406ec67
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-27 23:59:55 +0300

    Update useCharacterList to keepPreviousData

--- src/modules/characters/hooks/useCharacterList.ts
+++ src/modules/characters/hooks/useCharacterList.ts
@@ -5,11 +5,16 @@ import { ApiListResponse } from 'modules/core/core.interfaces';
 import { Character } from '../character.interfaces';
 
 export const useCharacterList = ({ page }: { page: number }) => {
-  return useQuery(['characters', 'page', page], () =>
-    axios
-      .get<ApiListResponse<Character[]>>(
-        `https://rickandmortyapi.com/api/character?page=${page}`
-      )
-      .then((res) => res.data)
+  return useQuery(
+    ['characters', 'page', page],
+    () =>
+      axios
+        .get<ApiListResponse<Character[]>>(
+          `https://rickandmortyapi.com/api/character?page=${page}`
+        )
+        .then((res) => res.data),
+    {
+      keepPreviousData: true,
+    }
   );
 };
