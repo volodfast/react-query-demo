1e6a9aa86ff326955218af04b6b92c80f289e279
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-28 23:40:49 +0300

    Update LocationList component

--- src/modules/location/components/LocationList.interface.ts
+++ src/modules/location/components/LocationList.interface.ts
@@ -1,3 +1,5 @@
+import { Location } from '../location.interfaces';
+
 export type LocationListProps = {
   list: Location[];
 };
--- src/modules/location/components/LocationList.tsx
+++ src/modules/location/components/LocationList.tsx
@@ -4,8 +4,22 @@ import { LocationListProps } from './LocationList.interface';
 // styles
 import { LocationListContainer } from './LocationList.styled';
 
-const LocationList: FC<LocationListProps> = () => {
-  return <LocationListContainer>Location list container</LocationListContainer>;
+const LocationList: FC<LocationListProps> = ({ list }) => {
+  return (
+    <LocationListContainer>
+      <h3>Location list</h3>
+
+      {list.map((location) => {
+        return (
+          <div key={location.id}>
+            <div>Id: {location.id}</div>
+            <div>Name: {location.name}</div>
+            <div>Type: {location.type}</div>
+          </div>
+        );
+      })}
+    </LocationListContainer>
+  );
 };
 
 export default LocationList;
