764263856692cfe739b63fdc517d41ec09fe48db
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-28 23:30:59 +0300

    Update useLocationList query hook

--- src/modules/location/hooks/useLocationList.ts
+++ src/modules/location/hooks/useLocationList.ts
@@ -1,19 +1,20 @@
 import axios from 'axios';
 import { useInfiniteQuery } from 'react-query';
 // interfaces
+import { ApiListResponse } from 'modules/core/core.interfaces';
 import { Location } from '../location.interfaces';
 
-const fetchLocationList: any = ({ page = 1 }) => {
+const fetchLocationList = ({ pageParam = 1 }) => {
   return axios
-    .get<Location[]>(`https://rickandmortyapi.com/api/location?page=${page}`)
+    .get<ApiListResponse<Location[]>>(
+      `https://rickandmortyapi.com/api/location?page=${pageParam}`
+    )
     .then((res) => res.data);
 };
 
 export const useLocationList = () => {
   return useInfiniteQuery('locations', fetchLocationList, {
-    getNextPageParam: (lastPage, pages) => {
-      console.log('lastPage', lastPage);
-      console.log('pages', pages);
-    },
+    getNextPageParam: (lastPage, pages) =>
+      lastPage.info.next && pages.length + 1,
   });
 };
