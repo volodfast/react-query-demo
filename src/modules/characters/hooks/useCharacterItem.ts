9951771db535eb4b06255dfbf677e3c3c1448816
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-10-30 02:41:41 +0300

    Refactor custom query hooks

--- src/modules/characters/hooks/useCharacterItem.ts
+++ src/modules/characters/hooks/useCharacterItem.ts
@@ -3,15 +3,13 @@ import { useQuery } from 'react-query';
 // interfaces
 import { Character } from '../character.interfaces';
 
+const fetchCharacter = (id: number) =>
+  axios
+    .get<Character>(`https://rickandmortyapi.com/api/character/${id}`)
+    .then((res) => res.data);
+
 export const useCharacterItem = (id: number) => {
-  return useQuery(
-    ['character', id],
-    () =>
-      axios
-        .get<Character>(`https://rickandmortyapi.com/api/character/${id}`)
-        .then((res) => res.data),
-    {
-      enabled: !!id,
-    }
-  );
+  return useQuery(['character', id], () => fetchCharacter(id), {
+    enabled: !!id,
+  });
 };
--- src/modules/characters/hooks/useCharacterList.ts
+++ src/modules/characters/hooks/useCharacterList.ts
@@ -4,15 +4,17 @@ import { useQuery } from 'react-query';
 import { ApiListResponse } from 'modules/core/core.interfaces';
 import { Character } from '../character.interfaces';
 
+const fetchCharacterList = ({ page }: { page: number }) =>
+  axios
+    .get<ApiListResponse<Character[]>>(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    )
+    .then((res) => res.data);
+
 export const useCharacterList = ({ page }: { page: number }) => {
   return useQuery(
     ['characters', 'page', page],
-    () =>
-      axios
-        .get<ApiListResponse<Character[]>>(
-          `https://rickandmortyapi.com/api/character?page=${page}`
-        )
-        .then((res) => res.data),
+    () => fetchCharacterList({ page }),
     {
       keepPreviousData: true,
     }
